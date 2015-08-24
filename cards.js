9f6f7135fe32c8f1edbc1039a1abb8f9fdb35021
Dan Bauer <dsbauer@gmail.com>
2015-08-23 21:29:12 -0700

    Rewrote as constructor with closures

--- cards.js
+++ cards.js
@@ -1,80 +1,80 @@
-var makeCard = // receive factory with external name `makeCard`
+var Card = // receive Ctor with external name `Card`
 (function () { //begin IIFE...
 
-    // The factory itself:
-    function makeCard(id) {  //makeCard is also IIFE's internal name
-        if (!isValidID(id))
-            return null;
-        var card = {
-            id:id,  //personal property
-            // links to shared methods, defined below:
-            rank : rank,
-            suit : suit,
-            color: color,
-            name : name
-        };
-        return card;
-    };
+	// The ctor itself:
+	function Card(id) {  //Card is also IIFE's internal name
+		if (!isValidID(id))
+			return null;
+
+		// Personal methods:
+		this.rank = function() { // --> 1..13
+			return Math.floor(id/4)+1;
+		};
+		this.suit = function() { // --> 1..4
+			return (id%4)+1;
+		};
+
+		// links to shared methods, defined below:
+		this.color= color;
+		this.name = name;
+	};
+
 //------------------
 // Private resources (internal use only)
 //------------------
 
-    var isValidID = function(num) { // Returns--> true, false
-        return ((typeof num)==="number") //correct type
-                && (num%1 === 0)        //integer
-                && num>=0 && num<=51;   //in range
-    };
+	var isValidID = function(num) { // Returns--> true, false
+		return ((typeof num)==="number") //correct type
+			&& (num%1 === 0)        //integer
+			&& num>=0 && num<=51;   //in range
+	};
 
-    var rankNames = ['','Ace','Two','Three','Four','Five','Six','Seven',
-                        'Eight','Nine','Ten','Jack','Queen','King'];
+	var rankNames = ['','Ace','Two','Three','Four','Five','Six','Seven',
+						'Eight','Nine','Ten','Jack','Queen','King'];
+	var rankAbbrs = ['','A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
-    var suitNames = ['','Hearts','Diamonds','Spades','Clubs'];
+	var suitNames = ['','Hearts','Diamonds','Spades','Clubs'];
+	var suitAbbrs = ['','H','D','S','C'];
 
 //-----------------------
 // Methods to be called through factory:
 //-----------------------
 
-    makeCard.isCard = function(card) { // Returns --> true, falsish
-        return card && (typeof card === 'object') // check for null or primitive
-            && (card.name === name) // check at least one method
-            && ('id' in card) && isValidID(card.id); //check id
-    };
+	Card.isCard = function(card) { // Returns --> true, falsish
+		return card && (card instanceof Card); // check for null or primitive
+	};
 
 //-----------------------------
-// Methods called though instances (where 'this' means the instance):
+// Shared Instance Methods
 //-----------------------------
 
-    var rank = function() { // --> 1..13, NaN
-        return Math.floor(this.id/4)+1;
-    };
+	var color = function() { // -->"red,"black", NaN
+		var suitVal=this.suit();
+		return ((suitVal<3)? "red": "black");
+	};
 
-    var suit = function() { // --> 1..4, NaN
-        return (this.id%4)+1;
-    };
-   
-    var color = function() { // -->"red,"black", NaN
-        var suitVal=this.suit();
-        return suitVal && ((suitVal<3)? "red": "black");
-    };
-
-    var name = function() { //--> string, NaN
-        var rankVal = this.rank();
-        var suitVal = this.suit();
-        return rankVal && suitVal &&
-            (rankNames[rankVal]+' of '+suitNames[suitVal]);
+	var name = function() { //--> string, NaN
+		var rankVal = this.rank();
+		var suitVal = this.suit();
+		return rankNames[rankVal]+' of '+suitNames[suitVal]);
+	};
+	var shortName = function() { //--> string, NaN
+		var rankVal = this.rank();
+		var suitVal = this.suit();
+		return rankAbbrs[rankVal]+suitAbbrs[suitVal]);
     };
 
 
-    // Use factory to create full set:
-    makeCard.fullSet = [];
-    for (var id=0; id<52; ++id) {
-        makeCard.fullSet.push(makeCard(id));
-    }
 
-    return makeCard;  //return factory function, product of IIFE's work
+	// Use factory to create full set:
+	Card.fullSet = [];
+	for (var id=0; id<52; ++id) {
+		Card.fullSet.push(new Card(id));
+	}
 
-})(); //end IIFE definition and do it now!
+	return Card;  //return factory function, product of IIFE's work
 
+})();
 
 
 
