85cd74c4fcfc50172303f2774104e38e1f295274
Dan Bauer <dsbauer@gmail.com>
2015-08-23 21:30:38 -0700

    Rewrote as constructor with prototype methods

--- cards.js
+++ cards.js
@@ -1,80 +1,76 @@
-var makeCard = // receive factory with external name `makeCard`
-(function () { //begin IIFE...
+var Card = (function () { //begin IIFE...
 
-    // The factory itself:
-    function makeCard(id) {  //makeCard is also IIFE's internal name
-        if (!isValidID(id))
-            return null;
-        var card = {
-            id:id,  //personal property
-            // links to shared methods, defined below:
-            rank : rank,
-            suit : suit,
-            color: color,
-            name : name
-        };
-        return card;
-    };
+	// The ctor itself:
+	function Card(id) {  //Card is also IIFE's internal name
+		if (!isValidID(id))
+			return null;
+		this.id=id,  //personal property
+	};
 //------------------
 // Private resources (internal use only)
 //------------------
 
-    var isValidID = function(num) { // Returns--> true, false
-        return ((typeof num)==="number") //correct type
-                && (num%1 === 0)        //integer
-                && num>=0 && num<=51;   //in range
-    };
+	function isValidID(num) { // Returns--> true, false
+		return ((typeof num)==="number") //correct type
+			&& (num%1 === 0)        //integer
+			&& num>=0 && num<=51;   //in range
+	};
 
-    var rankNames = ['','Ace','Two','Three','Four','Five','Six','Seven',
-                        'Eight','Nine','Ten','Jack','Queen','King'];
+	var rankNames = ['','Ace','Two','Three','Four','Five','Six','Seven',
+	'Eight','Nine','Ten','Jack','Queen','King'];
 
-    var suitNames = ['','Hearts','Diamonds','Spades','Clubs'];
+	var suitNames = ['','Hearts','Diamonds','Spades','Clubs'];
 
 //-----------------------
-// Methods to be called through factory:
+// Class Methods
 //-----------------------
 
-    makeCard.isCard = function(card) { // Returns --> true, falsish
-        return card && (typeof card === 'object') // check for null or primitive
-            && (card.name === name) // check at least one method
-            && ('id' in card) && isValidID(card.id); //check id
-    };
+	Card.isCard = function(card) { // Returns --> true, falsish
+		return card && (typeof card === 'object') // check for null or primitive
+			&& ('id' in card) && (card.hasOwnProperty('id'))
+			&& isValidID(card.id); //check id
+	};
 
 //-----------------------------
-// Methods called though instances (where 'this' means the instance):
+// Inherited Instance Methods
 //-----------------------------
 
-    var rank = function() { // --> 1..13, NaN
-        return Math.floor(this.id/4)+1;
-    };
+	Card.prototype.rank = function() { // --> 1..13, NaN
+		return Math.floor(this.id/4)+1;
+	};
 
-    var suit = function() { // --> 1..4, NaN
-        return (this.id%4)+1;
-    };
-   
-    var color = function() { // -->"red,"black", NaN
-        var suitVal=this.suit();
-        return suitVal && ((suitVal<3)? "red": "black");
-    };
+	Card.prototype.suit = function() { // --> 1..4, NaN
+		return (this.id%4)+1;
+	};
 
-    var name = function() { //--> string, NaN
-        var rankVal = this.rank();
-        var suitVal = this.suit();
-        return rankVal && suitVal &&
-            (rankNames[rankVal]+' of '+suitNames[suitVal]);
-    };
+	Card.prototype.color = function() { // -->"red,"black", NaN
+		var suitVal=this.suit();
+		return suitVal && ((suitVal<3)? "red": "black");
+	};
+
+	Card.prototype.name = function() { //--> string, NaN
+		var rankVal = this.rank();
+		var suitVal = this.suit();
+		return rankVal && suitVal &&
+			(rankNames[rankVal]+' of '+suitNames[suitVal]);
+	};
 
 
-    // Use factory to create full set:
-    makeCard.fullSet = [];
-    for (var id=0; id<52; ++id) {
-        makeCard.fullSet.push(makeCard(id));
-    }
 
-    return makeCard;  //return factory function, product of IIFE's work
+	// Use factory to create full set:
+	Card.fullSet = [];
+	for (var id=0; id<52; ++id) {
+		Card.fullSet[id] = new Card(id);
+	}
 
+	return Card;  //return Ctor
 })(); //end IIFE definition and do it now!
 
+
+// Export as NPM-style module
+if (typeof module !== "undefined") {
+	module.exports = Card;
+}
 
 
 
